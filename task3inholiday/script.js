f882180410c42a4b843b9438620db5ae4791a397
codexi <144174028+noureyvzz@users.noreply.github.com>
2024-11-13 10:07:42 -0800

    task2-commit2

--- task3inholiday/script.js
+++
@@ -1,6 +0,0 @@
-const users = [];
-function registerUser() {
-    const name = document.getElementById("registerName").value;
-    const email = document.getElementById("registerEmail").value;
-    const password = document.getElementById("registerPassword").value;
-}
