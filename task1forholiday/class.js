310a25929ec52bcc010d8c70d57c6c0e8166ef1b
codexi <144174028+noureyvzz@users.noreply.github.com>
2024-11-13 10:41:15 -0800

    task1-classjs

--- task1forholiday/class.js
+++ task1forholiday/class.js
@@ -1,1 +1,15 @@
+export class ToDoItem {
+    constructor(id, title, isDone = false, createdAt = new Date()) {
+        this.id = id;
+        this.title = title;
+        this.isDone = isDone;
+        this.createdAt = createdAt;
+    }
+}
 
+export class ToDoList {
+    constructor() {
+        this.todos = [];
+    }
+
+}
